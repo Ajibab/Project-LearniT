de4eaa9ad20e0bb55364d5d67ced41735d54261a
Olaosebikan <ajibolaolaosebikan@yahoo.com>
2024-02-04 02:32:43 +0000

    User Endpoint continued development

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,6 +5,8 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import exceptions, serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import Token
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
 ##--imports from the model class-##
 from .models import User,UserProfile,PendingUser,Token
@@ -57,3 +59,46 @@ class ChangePasswordSerializer(serializers.ModelSerializer):
         new_password_1 = self.validated_data["new_password"]
         user.set_password(new_password_1)
         user.save(update_fields=["password"])
+
+##-- Serializer to register User--##
+class RegisterUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    new_password_1 =serializers.CharField(write_only=True, required=True, validators=validate_password)
+    new_password_2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('id','first_name','surname','email','new_password_1','new_password_2')
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'first_name': {'required': True},
+            'surname': {'required': True},
+            'email': {'required': True}
+        }
+
+    def validate(self, attrs: dict):
+        ##-- Prevent email duplicacy and validate with password--##
+        email = attrs.get("email")
+        accepted_email = email.lower().strip()
+        if User.objects.filter(email_iexact=accepted_email).exists():
+            raise serializers.ValidationError({"email": "Email already exist"})
+        if attrs['new_password_1'] !=attrs['new_password_2']:
+            raise serializers.ValidationError({"password": "Incorrect password"})
+        return super().validate(attrs)
+    
+    def create(self, validated_data: dict):
+        user_data = {"email": validated_data.get("email"),
+                     "password": make_password(validated_data.get("new_password_1")),
+                     "first_name": validated_data.get("first_name"),
+                     "surname": validated_data.get("surname")
+                     }
+        user: User = User.objects.create(user_data)
+        return user
+
+
+
+
+
+
+
+        
