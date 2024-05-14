b8ed0a23b320a13d47fc09a8c3ffd86cab36cc20
root <root@Olaosebikan-Ajibola-Waliu-Governor>
2024-05-15 00:51:13 +0100

    updated task model

--- app/tasks/models.py
+++ app/tasks/models.py
@@ -14,39 +14,7 @@ from core.models import AuditableModel
 from django.utils.translation import gettext_lazy as _
 #from taggit.models import GenericUUIDTaggedItemBase, TaggedItemBase
 
-class RangeField(models.IntegerField):
-    description = _("Integer field with range")
-    def __init__(self,min_val,max_val,*args,**kwargs): 
-        self.min_val=min_val
-        self.max_val=max_val
-        super().__init__(*args, **kwargs)
-
-    def formfield(self, **kwargs):
-    
-        min_value=self.min_val
-        max_value=self.max_val
-        return super().formfield(**{
-            'min_value': min_value,
-            'max_value': max_value,
-            **kwargs,
-        })
-    def deconstruct(self):
-        name, path, args, kwargs = super().deconstruct()
- 
-        kwargs['min_val']=self.min_val
-        kwargs['max_val']=self.max_val
-
-        return name, path, args, kwargs     
-
-
-    def rel_db_type(self, connection):
-        print('yes here')
-        if connection.features.related_fields_match_type:
-            return self.db_type(connection)
-        else:
-            return models.IntegerField().db_type(connection=connection)
-
-class QuizCategory(AuditableModel):
+class Quiz(AuditableModel):
     module = models.OneToOneField("lms.Module",related_name="module_task",on_delete=models.CASCADE,blank=True,null=True)
     task = models.CharField(max_length=255)
     creator = models.ForeignKey("user.User",related_name='created_tasks',on_delete=models.CASCADE)
@@ -92,63 +60,65 @@ class QuizCategory(AuditableModel):
     
     def save(self, *args,**kwargs):
         self.slug = slugify(self.task)
-        super(QuizCategory,self).save(*args,**kwargs)
+        super(Quiz,self).save(*args,**kwargs)
 
 
-class SetOfQuestions(models.Model):
+class MultipleChoiceQuestions(models.Model):
     """This model saves the multiple choice questions,
     options and the correct options"""
-    tasks = models.ForeignKey('QuizCategory',on_delete=models.CASCADE,related_name='tasks_questions')
-    slug = models.SlugField(null=True, blank=True, max_length=100)
-    question = models.TextField (default='')#(max_length=200,blank=True,null=True)
-    option_1 = models.CharField(max_length=1024,blank=True,null=True)
-    option_2 = models.CharField(max_length=1024,blank=True,null=True)
-    option_3 = models.CharField(max_length=1024,blank=True,null=True)
-    option_4 = models.CharField(max_length=1024,blank=True,null=True)
-    has_project = models.BooleanField(default=False)
-    points = models.PositiveIntegerField(default=3,verbose_name='points')
-    answer = RangeField(min_val=1,max_val=4,default=1,verbose_name='correct option')
-    project = models.FileField(storage='project-questions/',blank=True,null=True)
+    quiz = models.ForeignKey(Quiz,related_name='questions',on_delete=models.CASCADE)
+    prompt_question = models.CharField(max_length=225,blank=True,null=True)
+    has_audio = models.BooleanField(default=False)
+    recorded_audio_question = models.FileField(storage='audio-questions/',blank=True,null=True)
+
+    class Meta:
+        ordering = ['id']
 
     def __str__(self) -> str:
-        return f'{self.question}'
-    
-    def get_answers(self):
-        multiple_choice_answer = list(Answer.objects.filter(question=self))
-        data = []
-        random.shuffle(multiple_choice_answer)
-        for choices in multiple_choice_answer:
-            data.append({
-                'answer': choices.answer,
-                'is_correct': choices.is_correct
-            })
-    
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.question[:60])
-        super(SetOfQuestions,self).save(*args,**kwargs)
+        return f'{self.prompt_question}'
 
 class Answer(AuditableModel):
-    question = models.ForeignKey(SetOfQuestions,related_name='question_answer',on_delete=models.CASCADE)
-    text = models.CharField(max_length=1025)
-    is_correct =models.BooleanField(default=False)
-    has_project = models.BooleanField(default=False)
-    project_solution_upload = models.FileField(storage='project-solution',blank=True,null=True)
+    question = models.ForeignKey(MultipleChoiceQuestions,related_name='question_answer',on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+    has_audio =models.BooleanField(default=False)
+    audio_record_answer = models.FileField(storage='audio-answers/',blank=True,null=True)
 
     def __str__(self) -> str:
         return f'{self.text}'
     
-class QuizPerformance(AuditableModel):
+
+    def __str__(self) -> str:
+        return f'{self.text}'
+    
+class UserAttemptActivity(AuditableModel):
     """Presents the performance of each user
     that completed a task"""
     user = models.ForeignKey('user.User',on_delete=models.CASCADE,related_name='task_performance')
-    quiz = models.ForeignKey(QuizCategory,on_delete=models.CASCADE,related_name='task_performance')
+    quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE,related_name='task_performance')
     score = models.FloatField()
+    date_taken = models.DateTimeField(auto_now_add=True)
     percentage_score = models.FloatField(default=0.0)
 
     def __str__(self) -> str:
         return f'{self.quiz.task} - {self.user}'
     def __str__(self) -> str:
         return f"{self.user}, you scored {self.score} in {self.quiz.task}"
+    
+class TheoryQuiz(AuditableModel):
+    module = models.OneToOneField("course.Module",related_name="module_quiz",on_delete=models.CASCADE,blank=True,null=True)
+    quiz_name = models.CharField(max_length=255)
+    created_by = models.ForeignKey("user.User",related_name="created_quiz",on_delete=models.CASCADE)
+    question = models.TextField()
+
+class TheoryQuizAttempt(AuditableModel):
+    user =models.ForeignKey("user.User", on_delete=models.CASCADE,related_name='taken_quiz')
+    theory_quiz = models.ForeignKey(TheoryQuiz, on_delete=models.CASCADE, related_name='taken_quizzes')
+    score = models.FloatField()
+    date_taken = models.DateTimeField(auto_now_add=True)
+    percentage_score = models.FloatField(default=0.00)
+    submitted_ansert = models.FileField()
+    
 
 
 
